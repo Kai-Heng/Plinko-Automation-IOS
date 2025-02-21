57fd3896c7745ef95126ba913eae31594b044b49
Kai-Heng <gankaiheng@gmail.com>
2025-02-21 11:08:35 -0600

    Initial Commit

---
+++ Plinko Automation IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plinko Automation IOS
+//
+//  Created by Kai Heng on 2/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plinko Automation IOS/Plinko_Automation_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plinko_Automation_IOSApp.swift
+//  Plinko Automation IOS
+//
+//  Created by Kai Heng on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Plinko_Automation_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
